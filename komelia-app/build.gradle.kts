e0666b3abd22bb2dfa0f9466485e2ea16c3f908f
sakurasm <1514383518@qq.com>
2026-05-31 00:35:42 +0800

    chore: bump app version to 0.18.9

--- komelia-app/build.gradle.kts
+++ komelia-app/build.gradle.kts
@@ -134,7 +134,7 @@ android {
         applicationId = "io.github.zhengningning.komelia"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 22
+        versionCode = 23
         versionName = libs.versions.app.version.get()
 
         val enableSelfUpdates = when (androidVariant) {
